29f237981083e8fc803414bfa3720ba591ab1350
raltma <rait.altmae@gmail.com>
2023-03-25 22:13:35 +0200

    refactor: fixed error class styling

--- objects/Error.php
+++ objects/Error.php
@@ -1,14 +1,17 @@
-<?php 
+<?php
 
 namespace spinTek\objects;
-class Error {
-    public static function showJsonError(string $errorMessage, int $http_code = 400){
-        $messageArray=[
+
+class Error
+{
+    public static function showJsonError(string $errorMessage, int $http_code = 400): void
+    {
+        $messageArray = [
             'errorMessage' => $errorMessage
         ];
         echo json_encode($messageArray);
         http_response_code($http_code);
         exit;
     }
-    
+
 }
